7ca005ffc98720d1763c2e43a8eaf755bc9129a6
tamaz bagdavadze <tamazbagdavadze@gmail.com>
2016-03-18 16:11:40 -0800

    add insertion sort

---
+++ insertion sort.js
@@ -0,0 +1,14 @@
+
+function insertionSort(arr){
+    var length = arr.length;
+    for(let i = 1; i < length; i++){
+       for(let j = i + 1; j < length; j++){
+           if(arr[i] > arr[j] ){
+               arr[i] = arr[i] + arr[j];
+               arr[j] = arr[i] - arr[j];
+               arr[i] = arr[i] - arr[j];
+           }
+       }
+    }
+    return arr;
+}
