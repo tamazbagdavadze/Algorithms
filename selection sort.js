5793c890e77f9e7b9a7fe5dfee98aebaf3d2ac3f
tamaz bagdavadze <tamazbagdavadze@gmail.com>
2016-03-18 16:20:59 -0800

    add selection sort

---
+++ selection sort.js
@@ -0,0 +1,20 @@
+
+function selectionSort(arr){
+    var length = arr.length;
+    for(let i = 0; i < length - 1; i++){
+        let min = i;
+        for(let j = i + 1; j < length; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+
+        if(min != i){
+            arr[i] = arr[i] + arr[min];
+            arr[min] = arr[i] - arr[min];
+            arr[i] = arr[i] - arr[min];
+        }
+
+    }
+    return arr;
+}
