69ea088fb5bcf593d05710a5a7e95dd856e3ad5a
tamaz bagdavadze <tamazbagdavadze@gmail.com>
2016-03-24 11:59:09 -0800

    add all sub combs

---
+++ all sub combinations.js
@@ -0,0 +1,16 @@
+function f(n){
+    var arr = new Array(n+1).fill(0);
+
+    while(arr[n] == 0){
+       console.log(arr.slice(0,n));
+       var j = 0;
+       arr[j]++;
+       while(arr[j] == 2){
+          arr[j]=0;
+          arr[j+1]++;
+          j++;
+       }
+    }
+}
+
+f(4);
