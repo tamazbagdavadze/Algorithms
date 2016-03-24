5d936227167e5b89fbd52d59666d5bc283c4a0e3
tamaz bagdavadze <tamazbagdavadze@gmail.com>
2016-03-24 11:43:14 -0800

    fix perms with duplications

--- permutations with duplications.js
+++ permutations with duplications.js
@@ -1,15 +1,15 @@
-//TODO fix
+//TODO check
 
-function f(m,n){
+function f(m,n,k){
   var a = new Array(m+1).fill(0);
-   
+
   while(a[m] == 0){
      console.log(a.slice(0,m));
      a[0]++;
 
      var j = 0;
-     while(a[j] == n-1)
-     { 
+     while(a[j] == k)
+     {
         a[j] = 0;
          a[j+1]++;
         j++;
@@ -18,4 +18,4 @@ function f(m,n){
 
 }
 
-f(4,6);
+f(4,6,3);
